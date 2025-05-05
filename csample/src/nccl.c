2add5a152dc33d3a1417a8afb1b7eb3ba56fd5f0
arulselvan1234@gmail.com <arulselvan1234@gmail.com>
2025-05-05 17:37:06 +0000

    clock cycles

--- csample/src/nccl.c
+++ csample/src/nccl.c
@@ -1,16 +1,18 @@
 #include "nccl.h"
-#include "time.h"
-
-void sleep_us(unsigned long microseconds)
-{
-    struct timespec ts;
-    ts.tv_sec = microseconds / 1000000ul;            // whole seconds
-    ts.tv_nsec = (microseconds % 1000000ul) * 1000;  // remainder, in nanoseconds
-    nanosleep(&ts, NULL);
-}
 
 ncclResult_t ncclAllReduce(const void* sendbuff, void* recvbuff, size_t count, ncclDataType_t datatype, ncclRedOp_t op,
                            ncclComm_t comm, cudaStream_t stream) {
-    sleep_us(1000 * 50);
-    return ncclSuccess;
+    clock_t start = clock();
+    clock_t now;
+    for (;;) {
+        now = clock();
+        clock_t cycles = now > start ? now - start : now + (0xffffffff - start);
+        if (cycles >= 10000) {
+            break;
+        }
+    }
+// Stored "now" in global memory here to prevent the
+// compiler from optimizing away the entire loop.
+    *global_now = now;
+    return ncclSystemError;
 }
